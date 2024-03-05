e874926810a7de2673cac2c5533b9400f0f5e9b4
Erick Valencia <evalencia0911@gmail.com>
2024-03-05 14:21:44 -0700

    most characters done

--- textgame.py
+++ textgame.py
@@ -14,6 +14,8 @@ class Character:
     def __ini__(self, name):
         # define your name
         self.name = name
+        # define hp between 1-20
+        self.hp = random.randint(1, 20)
         # random str between 4-8
         self.strength = random.randint(4, 8)
         # random def between 1-4
@@ -23,6 +25,17 @@ class Character:
 
     # define attack. Arg of self and other
     def attack(self, other):
+        # random die role to see if you successfully hit
+        if(random.randint(1, 10) <= self.accuracy):
+            # how much damage the hit will cause
+            damage = self.strength - other.defense
+            # how much hp is reduced by the damage
+            other.hp -= damage
+            # print response for damage done and how much hp is left
+            print(f"${self.name} hit ${other.name} for ${damage} damage, they have ${other.hp} left!")
+        else:
+            # print response for failing an attack
+            print(f"${self.name} fails to attack ${other.name}")
 
 
 # FUNCTIONS
